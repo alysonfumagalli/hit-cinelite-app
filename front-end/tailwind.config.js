7321cc25dc81e852e8083cc54d9c349167f661c1
Alyson Fumagalli <alysoncesarfumagalli@gmail.com>
2025-10-27 08:43:08 -0300

    chore: configure project colors

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Iris', 'sans-serif'],
+      },
+      colors: {
+        primary: 'var(--primary-color)',
+        accent: 'var(--accent-color)',
+        'background-primary': 'var(--background-primary-color)',
+        'background-secondary': 'var(--background-secondary-color)',
+        border: 'var(--border-color)',
+        title: 'var(--title-color)',
+        paragraph: 'var(--paragraph-color)',
+        error: 'var(--error-color)',
+        star: 'var(--star-color)',
+      }
+    },
   },
   plugins: [],
 }
